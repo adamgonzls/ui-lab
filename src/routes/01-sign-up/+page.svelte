edc1834be0998cf4788e07b0c16c92a7950e89ee
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-19 14:18:41 -0700

    content: add basic content to test routing functionality

--- src/routes/01-sign-up/+page.svelte
+++ src/routes/01-sign-up/+page.svelte
@@ -1,5 +1,11 @@
-<h2>Sign up page</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ad velit consectetur soluta, esse laboriosam, quidem ullam, iusto a facere aliquid modi eaque dolorem accusamus quos nihil earum perspiciatis vero?</p>
+<script>
+  import name from "../+page.svelte"
+</script>
+
+<main>
+  <h2>Sign up page</h2>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ad velit consectetur soluta, esse laboriosam, quidem ullam, iusto a facere aliquid modi eaque dolorem accusamus quos nihil earum perspiciatis vero?</p>
+</main>
 
 <style>
   p {

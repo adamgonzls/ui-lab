7eedcd8882fd84e9e50b2660311424993e2420e0
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-20 09:16:54 -0700

    content: import images and add to projects array

--- src/routes/projectData.js
+++ src/routes/projectData.js
@@ -1,24 +1,30 @@
+import dogBeanie from "$lib/assets/images/dog-beanie-640w.jpg"
+import purse from "$lib/assets/images/purse-640w.jpg"
+import jacket from "$lib/assets/images/jacket-640w.jpg"
+import shoes from "$lib/assets/images/shoes-640w.jpg"
+console.log(dogBeanie)
+
 export const projects = [
   {
-    src: "src/lib/assets/images/dog-beanie-640w.jpg",
+    src: dogBeanie,
     link: "/ui-lab/1",
     name: "Sign Up Page",
     description: "A sign up page for a website",
   },
   {
-    src: "src/lib/assets/images/purse-640w.jpg",
+    src: purse,
     link: "/ui-lab/2",
     name: "Another Project",
     description: "Some important words about this project",
   },
   {
-    src: "src/lib/assets/images/jacket-640w.jpg",
+    src: jacket,
     link: "/ui-lab/3",
     name: "This is my 3rd Project",
     description: "This is a something something",
   },
   {
-    src: "src/lib/assets/images/shoes-640w.jpg",
+    src: shoes,
     link: "/ui-lab/4",
     name: "A Longer Title for my 4th Project",
     description: "This si a very complicated something or other",

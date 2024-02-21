48928728c3011ed1a645c23c17e3ea8ab68e8e79
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-21 07:49:31 -0700

    content: add projects data to projects array

--- src/routes/projectData.js
+++ src/routes/projectData.js
@@ -1,3 +1,4 @@
+import woman from "$lib/assets/images/woman-500w.jpg"
 import dogBeanie from "$lib/assets/images/dog-beanie-640w.jpg"
 import purse from "$lib/assets/images/purse-640w.jpg"
 import jacket from "$lib/assets/images/jacket-640w.jpg"
@@ -6,7 +7,7 @@ console.log(dogBeanie)
 
 export const projects = [
   {
-    src: dogBeanie,
+    src: woman,
     link: "/01-sign-up",
     name: "Sign Up Page",
     description: "A sign up page for a website",
@@ -14,19 +15,67 @@ export const projects = [
   {
     src: purse,
     link: "/ui-lab/2",
-    name: "Another Project",
+    name: "Credit Card Checkout",
     description: "Some important words about this project",
   },
   {
     src: jacket,
     link: "/ui-lab/3",
-    name: "This is my 3rd Project",
+    name: "Landing Page",
     description: "This is a something something",
   },
   {
     src: shoes,
     link: "/ui-lab/4",
-    name: "A Longer Title for my 4th Project",
-    description: "This si a very complicated something or other",
+    name: "Calculator",
+    description: "This is a very complicated something or other",
+  },
+  {
+    src: dogBeanie,
+    link: "/05",
+    name: "App Icon",
+    description: "A sign up page for a website",
+  },
+  {
+    src: purse,
+    link: "/06",
+    name: "User Profile",
+    description: "Some important words about this project",
+  },
+  {
+    src: jacket,
+    link: "/07",
+    name: "Settings",
+    description: "This is a something something",
+  },
+  {
+    src: shoes,
+    link: "/08",
+    name: "404 Page",
+    description: "This is a very complicated something or other",
+  },
+  {
+    src: dogBeanie,
+    link: "/09",
+    name: "Music Player",
+    description: "This is a very complicated something or other",
+  },
+  {
+    src: purse,
+    link: "/10",
+    name: "Social Share",
+    description: "Some important words about this project",
+  },
+  {
+    src: jacket,
+    link: "/11",
+    name: "Flash Message",
+    description: "This is a something something",
+  },
+  {
+    src: shoes,
+    link: "/#",
+    name: "E-Commerce Shop",
+    description: "This is a very complicated something or other",
   },
 ]

8e7632704952e721368e8eeaadf69b9bcfd7b4fb
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-26 14:24:57 -0700

    styles: add content and position elements

--- src/routes/12-ecommerce-shop/+page.svelte
+++ src/routes/12-ecommerce-shop/+page.svelte
@@ -3,7 +3,10 @@
   import armchair from "$lib/assets/images/modern-living-room-wooden-armchair-308w.png"
   import rockingChair from "$lib/assets/images/rocking-chair-315w.png"
   import roundTable from "$lib/assets/images/round-wooden-vintage-table-389w.png"
-  import redThrome from "$lib/assets/images/red-and-gold-throne-chair-392w.png"
+  import redThrone from "$lib/assets/images/red-and-gold-throne-chair-392w.png"
+  import concreteBench from "$lib/assets/images/concrete-wood-bench-327w.png"
+  import peachCouch from "$lib/assets/images/peach-couch-301w.png"
+  import shoppingBag from "$lib/assets/images/purse-wallet.svg"
 </script>
 
 <div class="content-container">
@@ -11,16 +14,16 @@
   <img class="img--rotate-2" src={armchair} alt="Cantilever chair">
   <div class="info-container">
     <div>
-      <nav>
+      <nav class="page-navigation">
         <a href="#">Furniture</a>
         <a href="#">Designers</a>
         <a href="#">Trends</a>
         <a href="#">Blog</a>
         <a href="#">Contact</a>
-        <a href="#">Shopping Cart</a>
+        <a class="cart" href="#"><img class="cart__image" src={shoppingBag} alt=""></a>
       </nav>
-      <div class="product-container">
-        <div class="data-container">
+      <div class="product__container">
+        <div class="product__info">
           <h1>Relax Seat</h1>
           <h2>Feel the essence of comfort</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta optio, quibusdam adipisci laboriosam, temporibus in voluptates esse sequi debitis ad ipsum! Sit molestias autem quidem labore doloribus necessitatibus debitis numquam?</p>
@@ -28,20 +31,36 @@
           <button>Add to cart</button>
         </div>
         <div>color</div>
-        <div class="product-image">
+        <div class="product__image">
           <img src={fabricChair} alt="">
         </div>
       </div>
       <div class="related-products">
         <img class="related-image" src={rockingChair} alt="">
         <img class="related-image" src={roundTable} alt="">
-        <img class="related-image" src={redThrome} alt="">
+        <img class="related-image" src={concreteBench} alt="">
+        <img class="related-image" src={redThrone} alt="">
+        <img class="related-image" src={peachCouch} alt="">
       </div>
     </div>
   </div>
 </div>
 
 <style>
+  a {
+    color: #111;
+    text-decoration: none;
+  }
+  .page-navigation {
+    display: flex;
+    gap: 80px;
+  }
+  .cart {
+    margin-left: auto;
+  }
+  .cart__image {
+    width: 20px;
+  }
   .content-container {
     position: relative;
     height: 100vh;
@@ -58,25 +77,28 @@
     z-index: 10;
     position: relative;
   }
-  .product-container {
+  .product__container {
     display: flex;
     justify-content: space-between;
     gap: 20px;
+    margin-top: 40px;
   }
-  .product-container > div {}
-  .data-container,
-  .product-image {
+  .product__info,
+  .product__image {
     flex: 1;
   }
   .related-products {
     position: absolute;
     left: 0;
+    bottom: -10%;
     display: flex;
-    gap: 80px;
+    width: 100%;
+    justify-content: space-between;
   }
   .related-image {
-    height: 100px;
+    width: 80px;
     transition: transform 0.2s;
+    object-fit: contain;
   }
   .related-image:hover {
     transform: scale(1.1);
@@ -89,8 +111,8 @@
   }
   .img--rotate-2 {
     position: absolute;
-    bottom: 0;
-    right: 0;
+    bottom: -5%;
+    right: -8%;
     z-index: 11;
   }
 </style>

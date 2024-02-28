0aee88b8a9d46f16ecfd79135e1ce92709a98979
Adam Gonzales <adamgonzales1@gmail.com>
2024-02-27 18:13:46 -0700

    add folder for ecommerce-shop components

---
+++ src/components/ecommerce-shop/AddToCartForm.svelte
@@ -0,0 +1,18 @@
+<form on:submit|preventDefault={addToCart}>
+  <slot></slot>
+</form>
+
+<script>
+  import {addCartItem, isCartOpen} from '../../cartStore.ts'
+
+  const hardCodedItemInfo = {
+    id: 'wood-chair',
+    name: 'Wooden Chair',
+    imageSrc: '$lib/assets/images/cantilever-chair-700w.png'
+  }
+
+  function addToCart() {
+    isCartOpen.set(true)
+    addCartItem(hardCodedItemInfo)
+  }
+</script>
---
+++ src/components/ecommerce-shop/CartFlyoutToggle.svelte
@@ -0,0 +1,5 @@
+<script>
+  import {isCartOpen} from '../../cartStore.ts'
+</script>
+
+<button on:click={() => isCartOpen.set(!$isCartOpen)}>Cart</button>

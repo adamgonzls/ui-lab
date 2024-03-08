6cfea4ee35c1f6902973084b137d52d0c7c1f1bc
Adam Gonzales <adamgonzales1@gmail.com>
2024-03-07 21:59:39 -0700

    content: add images

---
+++ src/lib/assets/images/compass.svelte
@@ -0,0 +1,13 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="#fff" stroke="#fff" viewBox="-4 0 32 32">
+  <path fill="#fff" fill-rule="evenodd" stroke="none" d="M12 15c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3Zm0-8c-2.761 0-5 2.238-5 5s2.239 5 5 5 5-2.238 5-5-2.239-5-5-5Zm0 22c-1.663.009-10-12.819-10-17C2 6.478 6.477 2 12 2s10 4.478 10 10c0 4.125-8.363 17.009-10 17Zm0-29C5.373 0 0 5.373 0 12c0 5.018 10.005 20.011 12 20 1.964.011 12-15.05 12-20 0-6.627-5.373-12-12-12Z"/>
+</svg>
+
+<style>
+  svg {
+    display: inline-block;
+    background-color: #000;
+    padding: .25rem;
+    height: 1.5rem;
+    border-radius: 5px;
+  }
+</style>
---
+++ src/lib/assets/images/document-icon.svelte
@@ -0,0 +1,13 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+  <path fill="#fff" fill-rule="evenodd" d="M4 1a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.414A2 2 0 0 0 17.414 6L13 1.586A2 2 0 0 0 11.586 1H4zm0 2h7.586L16 7.414V17H4V3zm2 2a1 1 0 0 0 0 2h5a1 1 0 1 0 0-2H6zm-1 5a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1zm1 3a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H6z"/>
+</svg>
+
+<style>
+  svg {
+    display: inline-block;
+    background-color: #000;
+    padding: .25rem;
+    height: 1.5rem;
+    border-radius: 5px;
+  }
+</style>

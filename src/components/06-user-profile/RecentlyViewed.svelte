93d177973ceda2c01c449fff068d8e17ef7ce2e1
Adam Gonzales <adamgonzales1@gmail.com>
2024-04-24 17:34:44 -0600

    feature: add removeUser button, css to style removeUser button

--- src/components/06-user-profile/RecentlyViewed.svelte
+++ src/components/06-user-profile/RecentlyViewed.svelte
@@ -1,15 +1,21 @@
 <script>
   export let userList
+  export let removeUser
 </script>
 
 <div class="recently-viewed__container">
   <h2 class="recently-viewed__header">Searched users</h2>
   <ul class="recently-viewed__list">
     {#each $userList as { login, avatar_url, name }, i}
-      <li class="recently-viewed__item">
-        <button on:click id={login}>
+      <li data-id={i} class="recently-viewed__item">
+        <button on:click class="recently-viewed__select" id={login}>
           <img class="recently-viewed__img" src={avatar_url} alt={name} />
           <span class="recently-viewed__login">{login}</span>
+          <button
+            id={`${i}_${login}`}
+            on:click={removeUser}
+            class="recently-viewed__delete">❌</button
+          >
         </button>
       </li>
     {/each}
@@ -30,7 +36,10 @@
     gap: 1rem;
     overflow: scroll;
   }
-  .recently-viewed__item button {
+  .recently-viewed__item {
+    position: relative;
+  }
+  .recently-viewed__select {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -45,5 +54,14 @@
   }
   .recently-viewed__login {
     font-size: 0.75rem;
+  }
+  .recently-viewed__delete {
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    cursor: pointer;
+    border: 2px solid red;
+    border-radius: 9999px;
+    background: none;
   }
 </style>

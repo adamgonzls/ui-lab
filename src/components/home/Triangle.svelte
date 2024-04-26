bf5e8acb3b6e3562caf368e31a2138ace6aa814d
Adam Gonzales <adamgonzales1@gmail.com>
2024-04-26 15:00:18 -0600

    content: create component of triangle shape

---
+++ src/components/home/Triangle.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let color = "red"
+  export let rotation = "0deg"
+  export let verticalAlign = "auto"
+  export let size = "75px"
+</script>
+
+<div
+  class="triangle"
+  style="--color:{color};--rotation:{rotation};--verticalAlign:{verticalAlign};--size:{size};"
+></div>
+
+<style>
+  .triangle {
+    width: 0;
+    height: 0;
+    border-bottom: var(--size) solid var(--color);
+    border-right: var(--size) solid transparent;
+    rotate: var(--rotation);
+    align-self: var(--verticalAlign);
+  }
+</style>

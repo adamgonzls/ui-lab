43c02fd6475a538e55353ec50182e5c608f2c323
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:23:25 -0600

    content: add component

---
+++ src/components/03-landing-page/VideoFeature.svelte
@@ -0,0 +1,28 @@
+<script>
+  import posterImg from "$lib/assets/images/jacket-640w.jpg"
+  export let videoSrc
+</script>
+
+<div class="marqee__item">
+  <video loop poster={posterImg} class="marquee__item egg"
+    ><source src={videoSrc} type="video/mp4" /></video
+  >
+</div>
+
+<style>
+  .marquee__item {
+    width: 150px;
+    height: 150px;
+    position: relative;
+  }
+
+  .egg {
+    object-fit: cover;
+    /* position: absolute; */
+    /* top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%; */
+    /* clip-path: polygon(100% 0, 88% 63%, 58% 94%, 46% 94%, 13% 63%, 0 0); */
+  }
+</style>

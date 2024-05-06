5828768154da541283036bc8301e2ce3db1367a6
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:21:33 -0600

    content: add component

---
+++ src/components/03-landing-page/ChoosePlan.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Circle from "./Circle.svelte"
+  import DropPin from "./DropPin.svelte"
+</script>
+
+<div class="marquee__item">
+  <div class="marquee__circle-container">
+    <Circle />
+    <h3 class="marquee__circle-text">
+      <div class="marquee__circle-drop-pin">
+        <DropPin />
+      </div>
+      Choose your own plan
+    </h3>
+  </div>
+</div>
+
+<style>
+  .marquee__circle-container {
+    width: 150px;
+    height: 150px;
+    position: relative;
+  }
+
+  .marquee__circle-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
+  }
+
+  .marquee__circle-drop-pin {
+    margin-left: auto;
+    margin-right: auto;
+    width: 25px;
+  }
+</style>

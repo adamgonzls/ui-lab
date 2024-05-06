39039cc74dcc83f493bcd75c76344977dbf11fa5
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:22:56 -0600

    content: add component

---
+++ src/components/03-landing-page/OurBlog.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Tear from "./Tear.svelte"
+</script>
+
+<div class="marquee__item">
+  <div class="marquee__shape-container">
+    <Tear />
+    <h3 class="marquee__text">Our blog</h3>
+  </div>
+</div>
+
+<style>
+  .marquee__shape-container {
+    width: 150px;
+    height: 150px;
+    position: relative;
+  }
+
+  .marquee__text {
+    position: absolute;
+    bottom: 20%;
+    left: 50%;
+    transform: translateX(-50%);
+    color: white;
+  }
+</style>

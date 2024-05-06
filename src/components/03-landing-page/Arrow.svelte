c2d20945336d3116d4e9aa4953fa7c6b5ddd1da1
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-06 16:33:38 -0600

    styles: allow color to be passed to component

--- src/components/03-landing-page/Arrow.svelte
+++ src/components/03-landing-page/Arrow.svelte
@@ -1,6 +1,10 @@
+<script>
+  export let fillColor = "#000000"
+</script>
+
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
   <path
-    fill="#ffffff"
+    fill={fillColor}
     d="M12 4c-.5523 0-1-.44772-1-1s.4477-1 1-1h8c1.1046 0 2 .89543 2 2v8c0 .5523-.4477 1-1 1s-1-.4477-1-1V5.39343L3.72798 21.6655c-.39052.3905-1.02369.3905-1.41421 0-.39053-.3906-.39053-1.0237 0-1.4143L18.565 4H12Z"
   />
 </svg>

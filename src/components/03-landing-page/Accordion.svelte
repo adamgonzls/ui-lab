8ae3d0a277f16b0a363d22e0ff88cb317715427c
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:21:08 -0600

    content: add shape

---
+++ src/components/03-landing-page/Accordion.svelte
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 200 200">
+  <path
+    fill="rgba(245, 215, 254, 1)"
+    d="M0 45.736 50 33l50 12.736L150 33l50 12.736V168l-50-12.736L100 168l-50-12.736L0 168V45.736Z"
+  />
+</svg>

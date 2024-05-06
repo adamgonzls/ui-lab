b6558c0dd6bcc3ec4b8654e1a532948e43288513
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-06 16:37:34 -0600

    content: add Arrow to component

--- src/components/03-landing-page/OurBlog.svelte
+++ src/components/03-landing-page/OurBlog.svelte
@@ -1,11 +1,15 @@
 <script>
   import Tear from "./Tear.svelte"
+  import Arrow from "./Arrow.svelte"
 </script>
 
 <div class="marquee__item">
   <div class="marquee__shape-container">
     <Tear />
     <h3 class="marquee__text">Our blog</h3>
+    <div class="marquee__arrow-container">
+      <Arrow fillColor="#ffffff" />
+    </div>
   </div>
 </div>
 
@@ -15,12 +19,18 @@
     height: 150px;
     position: relative;
   }
-
   .marquee__text {
     position: absolute;
-    bottom: 20%;
+    bottom: 25%;
     left: 50%;
     transform: translateX(-50%);
     color: white;
+    font-family: var(--header-font);
+  }
+  .marquee__arrow-container {
+    position: absolute;
+    top: 20%;
+    right: 25%;
+    width: 25px;
   }
 </style>

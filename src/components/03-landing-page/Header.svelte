6b02fd9068bd0a866d5e94eb5a80accfedb90acd
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:22:24 -0600

    content: add component

---
+++ src/components/03-landing-page/Header.svelte
@@ -0,0 +1,27 @@
+<nav>
+  <h1><a href="#">CircleUp</a></h1>
+  <ul class="pages">
+    <li>Features</li>
+    <li>Privacy</li>
+    <li>Help Centre</li>
+    <li>Blog</li>
+  </ul>
+  <div>
+    <a href="#">Log in</a>
+    <a href="#">Sign up</a>
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .pages {
+    display: flex;
+    list-style-type: none;
+    gap: 20px;
+  }
+</style>

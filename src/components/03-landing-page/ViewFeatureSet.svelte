8faa95b7f6f1aa104b8209b66b4c32975bb748fd
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:23:58 -0600

    content: add component content

---
+++ src/components/03-landing-page/ViewFeatureSet.svelte
@@ -0,0 +1,58 @@
+<script>
+  import Gear from "./Gear.svelte"
+  import Eye from "./Eye.svelte"
+  /**
+   * @type {string}
+   */
+  export let marqueeText
+  export let marqueeIcon
+  export let bgColor
+</script>
+
+<div class="marquee__item">
+  <div class="spin__container">
+    <div class="spin">
+      <Gear fillColor={bgColor} />
+    </div>
+    <h3 class="spin__text">
+      <div class="eye__container"><Eye /></div>
+      {marqueeText}
+    </h3>
+  </div>
+</div>
+
+<style>
+  .spin {
+    animation: spin 5s linear infinite;
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spin__container {
+    position: relative;
+    width: 150px;
+    height: 150px;
+  }
+
+  .eye__container {
+    margin-left: auto;
+    margin-right: auto;
+    width: 25px;
+  }
+
+  .spin__text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
+  }
+</style>

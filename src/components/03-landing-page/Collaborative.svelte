f57983e7cdbe54179f351c50c37fabc99333a065
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:21:45 -0600

    content: add component

---
+++ src/components/03-landing-page/Collaborative.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Accordion from "./Accordion.svelte"
+</script>
+
+<div class="marquee__item marquee__item--accordion">
+  <div class="accordion__container">
+    <Accordion />
+  </div>
+  <h3 class="accordion__text">Collaborative</h3>
+</div>
+
+<style>
+  .marquee__item--accordion {
+    position: relative;
+    height: 150px;
+    width: 150px;
+  }
+
+  .accordion__container {
+  }
+
+  .accordion__text {
+    position: absolute;
+    bottom: 20%;
+    left: 50%;
+    transform: translateX(-50%);
+    color: #3f3a72;
+  }
+</style>

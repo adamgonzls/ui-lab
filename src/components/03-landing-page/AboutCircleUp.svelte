8a20737e221561a8a91e4d01cd2f555f8ea7e57a
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:20:48 -0600

    content: add component content

---
+++ src/components/03-landing-page/AboutCircleUp.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Square from "./Square.svelte"
+</script>
+
+<div class="marquee__item">
+  <div class="marquee__square-container">
+    <Square />
+    <h3 class="marquee__text">About CircleUp</h3>
+  </div>
+</div>
+
+<style>
+  .marquee__square-container {
+    width: 150px;
+    height: 150px;
+    position: relative;
+  }
+
+  .marquee__text {
+    position: absolute;
+    bottom: 20%;
+    left: 5%;
+    color: white;
+    width: 50%;
+  }
+</style>

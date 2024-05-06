83c69b0334d660bea53bb07881cbc1c12b794f19
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-05 21:24:30 -0600

    content: add component content

---
+++ src/components/03-landing-page/ViewUpdates.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Gear from "./Gear.svelte"
+</script>
+
+<div class="marquee__item">
+  <div class="spin">
+    <Gear fillColor="#c5c1ff" />
+  </div>
+  <h3>View updates</h3>
+</div>

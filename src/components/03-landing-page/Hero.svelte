6ec38e6bee76304838bc548a04b76690f532148b
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-07 12:14:34 -0600

    styles: rename classes to be more BEM-like, css to make elements more mobile-responsive

--- src/components/03-landing-page/Hero.svelte
+++ src/components/03-landing-page/Hero.svelte
@@ -6,20 +6,20 @@
 </script>
 
 <div class="hero">
-  <div class="row">
+  <div class="hero__row">
     <h2 class="hero__text">Connect</h2>
     <span class="hero__dot">&#8226;</span>
     <span class="oval-image" style={`background-image: url(${SocialMan1})`}
     ></span>
     <h2 class="hero__text">Create</h2>
   </div>
-  <div class="row row--top-margin">
+  <div class="hero__row hero__row--top-margin">
     <h2 class="hero__text">Augment</h2>
     <span class="oval-image" style={`background-image: url(${SocialWoman1})`}
     ></span>
     <h2 class="hero__text">Your</h2>
   </div>
-  <div class="row row--top-margin">
+  <div class="hero__row hero__row--top-margin">
     <span class="oval-image" style={`background-image: url(${SocialWoman3})`}
     ></span>
     <h2 class="hero__text">Reality</h2>
@@ -27,7 +27,7 @@
     <span class="oval-image" style={`background-image: url(${SocialWoman2})`}
     ></span>
   </div>
-  <h3 class="hero__cta row">(Download now)</h3>
+  <h3 class="hero__cta hero__row">(Download now)</h3>
 </div>
 
 <style>
@@ -38,17 +38,19 @@
     align-items: center;
     color: var(--blue-zodiac);
   }
-  .row {
+  .hero__row {
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
-    gap: 10px;
+    justify-content: center;
+    gap: 0.25rem;
+  }
+  .hero__row--top-margin {
+    margin-top: 0.25rem;
   }
   .hero__dot {
     line-height: 0.8em;
-    font-size: 5rem;
-  }
-  .row--top-margin {
-    margin-top: 10px;
+    font-size: 3rem;
   }
   .oval-image {
     width: 80px;
@@ -59,17 +61,34 @@
   }
   .hero__text {
     font-family: var(--body-font);
-    font-size: 4rem;
+    font-size: 2rem;
     font-weight: 600;
     text-transform: uppercase;
     line-height: 0.8em;
   }
   .hero__cta {
     margin-top: 20px;
-    font-size: 5rem;
+    font-size: 3rem;
     font-family: var(--header-font);
     font-weight: 100;
     line-height: 1;
     color: var(--pepto);
+  }
+  @media (min-width: 500px) {
+    .hero__row {
+      gap: 0.625rem;
+    }
+    .hero__text {
+      font-size: 4rem;
+    }
+    .hero__cta {
+      font-size: 5rem;
+    }
+    .hero__row--top-margin {
+      margin-top: 0.625rem;
+    }
+    .hero__dot {
+      font-size: 5rem;
+    }
   }
 </style>

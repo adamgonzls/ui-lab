589fab748363df2418c6cfc22576797bed49d8e0
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-07 12:13:47 -0600

    refactor: create component from content

--- src/components/03-landing-page/Header.svelte
+++ src/components/03-landing-page/Header.svelte
@@ -1,19 +1,29 @@
+<script>
+  import BrandLogo from "./BrandLogo.svelte"
+  import UserActions from "./UserActions.svelte"
+</script>
+
 <nav>
-  <h1><a href="#">CircleUp</a></h1>
-  <ul class="nav__pages">
+  <div class="nav--desktop">
+    <BrandLogo />
+    <ul class="nav__page-links">
+      <li>Features</li>
+      <li>Privacy</li>
+      <li>Help Centre</li>
+      <li>Blog</li>
+    </ul>
+    <UserActions />
+  </div>
+  <ul class="nav__page-links--mobile">
     <li>Features</li>
     <li>Privacy</li>
     <li>Help Centre</li>
     <li>Blog</li>
   </ul>
-  <div class="nav__actions">
-    <a href="#">Log in</a>
-    <a class="nav__signup" href="#">Sign up</a>
-  </div>
 </nav>
 
 <style>
-  nav {
+  .nav--desktop {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -22,33 +32,26 @@
     padding-right: 1rem;
     padding-left: 1rem;
   }
-  nav h1 {
-    border-radius: 40px;
-    padding: 0.25rem 1rem;
-    font-family: var(--header-font);
-    background-color: #e5ffb5;
-  }
-  nav a {
-    text-decoration: none;
-    color: var(--blue-zodiac);
-  }
-  .nav__pages {
-    display: flex;
+  .nav__page-links {
+    padding-left: 0;
+    display: none;
     list-style-type: none;
     gap: 20px;
   }
-  .nav__actions {
+  .nav__page-links--mobile {
+    margin-top: 0.25rem;
+    padding-left: 1rem;
+    list-style-type: none;
     display: flex;
-    align-items: center;
-    gap: 20px;
-    border-radius: 40px;
-    padding: 0.25rem 1rem;
-    background-color: var(--white);
+    gap: 0.625rem;
+    color: var(--blue-zodiac);
   }
-  .nav__signup {
-    padding: 0.25rem 1rem;
-    border-radius: 40px;
-    background-color: var(--blue-zodiac);
-    color: var(--white);
+  @media (min-width: 700px) {
+    .nav__page-links {
+      display: flex;
+    }
+    .nav__page-links--mobile {
+      display: none;
+    }
   }
 </style>

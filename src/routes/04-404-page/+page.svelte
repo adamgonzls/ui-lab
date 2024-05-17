2c9fa27610f6da4dccafc10c3c0c5be6fe5edee0
Adam Gonzales <adamgonzales1@gmail.com>
2024-05-17 15:36:45 -0600

    content: add basic skeleton content

---
+++ src/routes/04-404-page/+page.svelte
@@ -0,0 +1,29 @@
+<div>
+  <nav>
+    <span>Company logo</span>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Careers</li>
+      <li>Contact Us</li>
+      <li>FAQ</li>
+    </ul>
+  </nav>
+  <main>
+    <div class="background-blob"></div>
+    <div>
+      <h1>Oops!</h1>
+      <p>Houston, we have a 404.</p>
+      <p>Page not found in this galaxy.</p>
+      <a href="/">Head back home</a>
+    </div>
+    <div>
+      <div>
+        <span>4</span>
+        <img src="" alt="The moon" />
+        <span>4</span>
+      </div>
+      <div class="astronaut"></div>
+    </div>
+  </main>
+</div>

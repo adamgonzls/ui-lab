bbb7a6ca835da18c716c6c74efeddce08456c1b4
Adam Gonzales <adamgonzales1@gmail.com>
2024-11-29 19:06:44 -0700

    refactor: update CityWeatherData interface with searchedCity, coord, and forecast, remove now unused functions mostUsedCountryCodes, sortedCountries, getLocationLatLong, remove now unused references to geocodingApi, weatherDataApi, add function getCurrentWeather, getWeatherForecast, style elements

--- src/routes/37-weather/+page.svelte
+++ src/routes/37-weather/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import { PUBLIC_OPENWEATHER_API_KEY } from "$env/static/public"
-  import { iso31661 } from "iso-3166"
-  import { iso31662 } from "iso-3166"
-  console.log(iso31661)
   import "../../styles.css"
   import "$lib/assets/fonts/stylesheet.css"
   import "$lib/assets/fonts/37-weather/stylesheet.css"
@@ -17,108 +14,31 @@
 
   interface CityWeatherData {
     name: string
+    searchedCity?: string
     dt: number
     weather: { description: string }[]
     main: { temp: number; feels_like: number; humidity: number }
     wind: { speed: number }
     visibility: number
+    coord?: { lat: number; lon: number }
+    forecast?: any
   }
 
   let debounceTimeout: number
   let cityName = ""
   let foundCities: City[] = []
-  let countryCode = ""
-  let stateCode = ""
 
   let cityWeatherData: CityWeatherData = {
     name: "",
+    searchedCity: "",
     dt: 0,
     weather: [{ description: "" }],
     main: { temp: 0, feels_like: 0, humidity: 0 },
     wind: { speed: 0 },
     visibility: 0,
+    coord: { lat: 0, lon: 0 },
   }
   let currentDate = ""
-  const mostUsedCountryCodes = [
-    "US",
-    "CA",
-    "AU",
-    "GB",
-    "DE",
-    "FR",
-    "BR",
-    "IT",
-    "ES",
-    "NL",
-  ]
-
-  $: sortedCountries = iso31661.sort((a, b) => {
-    // Check if 'a' or 'b' is in the topCountries list
-    const aIsTop = mostUsedCountryCodes.includes(a.alpha2)
-    const bIsTop = mostUsedCountryCodes.includes(b.alpha2)
-
-    // Move top countries to the top of the list
-    if (aIsTop && bIsTop) {
-      return (
-        mostUsedCountryCodes.indexOf(a.alpha2) -
-        mostUsedCountryCodes.indexOf(b.alpha2)
-      ) // Respect topCountries order
-    }
-    if (aIsTop) return -1 // 'a' is a top country, so it comes first
-    if (bIsTop) return 1 // 'b' is a top country, so it comes first
-
-    // Alphabetical sort for the rest
-    return a.alpha2.localeCompare(b.alpha2)
-  })
-
-  $: geocodingApi = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}${stateCode ? `,${stateCode}` : ""},${countryCode}&limit=3&appid=${PUBLIC_OPENWEATHER_API_KEY}`
-  $: weatherDataApi = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${PUBLIC_OPENWEATHER_API_KEY}`
-  $: filteredStates = iso31662.filter((state) => state.parent === countryCode)
-
-  function getLocationLatLong() {
-    // console.log(cityName)
-    // console.log(countryCode)
-    // console.log(geocodingApi)
-    fetch(geocodingApi)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("inside getLocationLatLong")
-        // console.log(data)
-        // get the weather data from the first element in the array
-        console.log(data[0])
-        const selectedCity = data[0]
-        const { lat, lon } = selectedCity
-        getWeatherData(lat, lon)
-      })
-  }
-
-  // function getWeatherData(lat: number, lon: number) {
-  //   console.log("Getting weather data")
-  //   weatherDataApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${PUBLIC_OPENWEATHER_API_KEY}&units=imperial`
-  //   fetch(weatherDataApi)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("inside getWeatherData")
-  //       console.log(data)
-  //       cityWeatherData = data
-  //       const date = new Date(cityWeatherData.dt * 1000)
-  //       currentDate = new Intl.DateTimeFormat("en-US", {
-  //         weekday: "long", // Full name of the day (e.g., "Friday")
-  //         day: "numeric", // Day of the month (e.g., "20")
-  //         month: "long", // Full name of the month (e.g., "January")
-  //       }).format(date)
-  //     })
-  // }
-
-  // function getCities() {
-  //   console.log("Getting cities")
-  //   fetch(geocodingApi)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("inside getCities")
-  //       console.log(data)
-  //     })
-  // }
 
   function formatDataCalculationDate(dt: number) {
     const date = new Date(dt * 1000)
@@ -160,33 +80,46 @@
     }, 1000) // Adjust the delay as needed
   }
 
-  async function fetchWeather(city) {
-    console.log("Fetching weather for", city)
-    const { lat, lon } = city
+  async function getCurrentWeather(selectedCity: City) {
+    const { lat, lon } = selectedCity
     const weatherDataApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${PUBLIC_OPENWEATHER_API_KEY}&units=imperial`
     try {
       const response = await fetch(weatherDataApi)
       const data = await response.json()
+      console.log(`You searched for ${selectedCity.name}`)
       cityWeatherData = data
+      cityWeatherData.searchedCity = selectedCity.name
+      if (cityWeatherData.coord) {
+        const forecastResponse = await getWeatherForecast(
+          cityWeatherData.coord.lat,
+          cityWeatherData.coord.lon
+        )
+        const forecastData = await forecastResponse.json()
+        cityWeatherData.forecast = forecastData
+      }
+      console.log(cityWeatherData)
       formatDataCalculationDate(cityWeatherData.dt)
     } catch (error) {
       console.log(error)
     }
   }
 
-  async function getData() {
-    console.log("Getting data")
-    getLocationLatLong()
+  async function getWeatherForecast(lat: number, lon: number) {
+    const forecastDataApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${PUBLIC_OPENWEATHER_API_KEY}&units=imperial`
+    return await fetch(forecastDataApi)
   }
 </script>
 
 <div class="page">
-  <main class="weather">
+  <main class="page__content">
+    <h1>WeatherVane</h1>
     <input
       id="cityNameInput"
+      class="input__city-name"
       bind:value={cityName}
       on:input={getCities}
       type="text"
+      placeholder="Enter a city name"
     />
     {#if foundCities.length > 0}
       <ul class="results">
@@ -194,7 +127,7 @@
           <li class="results__item">
             <button
               class="results__button"
-              on:click={() => fetchWeather(foundCity)}
+              on:click={() => getCurrentWeather(foundCity)}
             >
               {foundCity.name},{foundCity.state ? ` ${foundCity.state},` : ""}
               {foundCity.country}
@@ -203,24 +136,10 @@
         {/each}
       </ul>
     {/if}
-    <select id="countryCodeInput" bind:value={countryCode}>
-      {#each sortedCountries as country}
-        <option value={country.alpha2}>{country.name}</option>
-      {/each}
-    </select>
-    {#if countryCode}
-      <select id="stateCodeInput" bind:value={stateCode}>
-        {#each filteredStates as state}
-          <option value={state.code}>{state.name}</option>
-        {/each}
-      </select>
-    {/if}
-    <div>
-      <button on:click={getData}>Get Data</button>
-    </div>
     {#if cityWeatherData.name !== ""}
       <div class="weather-data">
-        <span class="weather__city">{cityWeatherData.name}</span>
+        <span class="weather__city">{cityWeatherData.searchedCity}</span>
+        <span class="weather__source">{cityWeatherData.name}</span>
         <span class="weather__date">{currentDate}</span>
         <span class="weather__description"
           >{cityWeatherData.weather[0].description}</span
@@ -286,6 +205,35 @@
     justify-content: center;
     align-items: center;
   }
+  .page__content {
+    border-radius: 0.5rem;
+    padding: 1.25rem;
+    background-color: white;
+    font-family: var(--body-font);
+  }
+  @media (min-width: 475px) {
+    .page__content {
+      width: 80%;
+    }
+  }
+  @media (min-width: 600px) {
+    .page__content {
+      width: 70%;
+    }
+  }
+  @media (min-width: 730px) {
+    .page__content {
+      width: 60%;
+    }
+  }
+  @media (min-width: 850px) {
+    .page__content {
+      width: 50%;
+    }
+  }
+  .input__city-name {
+    width: 100%;
+  }
   .results {
     display: flex;
     flex-wrap: wrap;
@@ -311,40 +259,21 @@
     color: white;
   }
   .weather-data {
-    font-family: var(--body-font);
     margin-left: auto;
     margin-right: auto;
     width: 100%;
     display: flex;
     flex-direction: column;
-    padding: 1.25rem;
-    border-radius: 0.5rem;
-    background-color: white;
+
     align-items: center;
-  }
-  @media (min-width: 475px) {
-    .weather {
-      width: 80%;
-    }
-  }
-  @media (min-width: 600px) {
-    .weather {
-      width: 70%;
-    }
-  }
-  @media (min-width: 730px) {
-    .weather {
-      width: 60%;
-    }
-  }
-  @media (min-width: 850px) {
-    .weather {
-      width: 50%;
-    }
   }
   .weather__city {
     font-size: 1.25rem;
     font-weight: bold;
+  }
+  .weather__source {
+    font-size: 1rem;
+    color: #222222;
   }
   .weather__date {
     margin-top: 1em;
